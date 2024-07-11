3b7bcfb19e9435d9855a3d113e6aaf61be185ac9
Thyago-ES <thyagorafael57@gmail.com>
2024-07-10 22:43:58 -0300

    Componente do Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,22 @@
+export function Footer() {
+	return (
+		<footer>
+			<div>
+				<span>DevGames</span>
+				<div>
+					<a href="#">&copy; DevGames 2024</a>
+					<a href="#">Privacy policy</a>
+					<a href="#">Cookies policy</a>
+					<a href="#">Terms of use</a>
+				</div>
+			</div>
+			<div>
+				<span>Updates right to your inbox</span>
+				<div>
+					<input type="text" placeholder="Email Address" />
+					<button>Send</button>
+				</div>
+			</div>
+		</footer>
+	);
+}
