7b97fea3c51221349573705eef5fbcc2f7956f85
Thyago-ES <thyagorafael57@gmail.com>
2024-07-10 22:44:53 -0300

    Componente dos Partners

---
+++ src/components/Partners/index.tsx
@@ -0,0 +1,9 @@
+import partners from "../../assets/images/partners.png";
+
+export function Partners() {
+	return (
+		<section>
+			<img src={partners} alt="Parceiros da empresa" />
+		</section>
+	);
+}
