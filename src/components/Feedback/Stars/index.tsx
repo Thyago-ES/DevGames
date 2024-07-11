71529c1a4e00992b9140394c2a7487844d3c4e71
Thyago-ES <thyagorafael57@gmail.com>
2024-07-10 22:43:27 -0300

    Estrelas de avaliação

---
+++ src/components/Feedback/Stars/index.tsx
@@ -0,0 +1,12 @@
+import star from "../../../assets/icons/star.png";
+export function Stars() {
+	return (
+		<div>
+			<img src={star} />
+			<img src={star} />
+			<img src={star} />
+			<img src={star} />
+			<img src={star} />
+		</div>
+	);
+}
