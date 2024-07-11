903d25548751f644c2e73cc2172c980361d612f1
Thyago-ES <thyagorafael57@gmail.com>
2024-07-10 22:45:33 -0300

    Componente do ProductView

---
+++ src/components/ProductView/index.tsx
@@ -0,0 +1,9 @@
+import productView from "../../assets/images/product-img.png";
+
+export function ProductView() {
+	return (
+		<section>
+			<img src={productView} alt="Produto" />
+		</section>
+	);
+}
