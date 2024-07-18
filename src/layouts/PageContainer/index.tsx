e5f9981684f4d86f320744ce18835c569c91395f
Thyago-ES <thyagorafael57@gmail.com>
2024-07-17 21:30:38 -0300

    classe adicionada para estilos

--- src/layouts/PageContainer/index.tsx
+++ src/layouts/PageContainer/index.tsx
@@ -1,7 +1,9 @@
+import "./styles.css";
+
 type ContainerProps = {
 	children: React.ReactNode;
 };
 
 export function PageContainer({ children }: ContainerProps) {
-	return <div>{children}</div>;
+	return <div className="container">{children}</div>;
 }
