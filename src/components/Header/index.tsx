069fdd8c81826fe6dd7876844eb254f045ff5e4b
Thyago-ES <thyagorafael57@gmail.com>
2024-07-17 21:27:49 -0300

    classes adicionadas para estilos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,13 @@
+import "./styles.css";
+
 export function Header() {
 	return (
-		<header>
-			<span>DevGames</span>
-			<nav>
-				<button>Sign up</button>
-				<button>Log in</button>
-			</nav>
+		<header className="header">
+			<span className="logo">DevGames</span>
+			<div className="header-bts">
+				<button className="btn simple-btn">Sign up</button>
+				<button className="btn cold-btn">Log in</button>
+			</div>
 		</header>
 	);
 }
