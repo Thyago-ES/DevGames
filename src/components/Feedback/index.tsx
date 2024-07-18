b25a790d13c33a1b98232520b9c9ff8e1d7ca17f
Thyago-ES <thyagorafael57@gmail.com>
2024-07-17 21:26:18 -0300

    classes adicionadas para estilos

--- src/components/Feedback/index.tsx
+++ src/components/Feedback/index.tsx
@@ -1,24 +1,27 @@
+import "./styles.css";
 import userImage from "../../assets/images/user-feedback.png";
 
 import { Stars } from "./Stars";
 export function Feedback() {
 	return (
-		<section>
-			<div>
+		<section className="feedback">
+			<section className="feedback-content">
 				<p>
 					“Wallet is a great product! All of my most important information is
 					there - credit cards, transit cards, boarding passes, tickets, and
 					more. And I don't need to worry because it's all in one place!
 					thanks!”
 				</p>
-				<div>
-					<img src={userImage} alt="Imagem do usuário" />
-					<div>
+				<div className="user-feedback">
+					<figure>
+						<img src={userImage} alt="Imagem do usuário" />
+					</figure>
+					<div className="user-details">
 						<span>Johnny Owens</span>
 						<Stars />
 					</div>
 				</div>
-			</div>
+			</section>
 		</section>
 	);
 }
