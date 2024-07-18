c09818b6e1a823630f333022fda422dd1ddfd6d4
Thyago-ES <thyagorafael57@gmail.com>
2024-07-17 21:22:58 -0300

    classes adicionadas para estilos

--- src/components/Action/index.tsx
+++ src/components/Action/index.tsx
@@ -1,16 +1,21 @@
+import "./styles.css";
 import actionImage from "../../assets/images/action-img.png";
 
 export function Action() {
 	return (
-		<section>
-			<div>
-				<h2>Questions? Let's talk</h2>
-				<p>
-					Contact us through our 24/7 live chat. We're always happy to help!
-				</p>
-				<button>Get started</button>
-			</div>
-			<img src={actionImage} alt="Imagem do action" />
+		<section className="action">
+			<header className="action-container">
+				<section className="action-content">
+					<h2 className="title">Questions? Let's talk</h2>
+					<p className="subtitle">
+						Contact us through our 24/7 live chat. We're always happy to help!
+					</p>
+				</section>
+				<button className="btn hot-btn">Get started</button>
+			</header>
+			<figure className="action-img">
+				<img src={actionImage} alt="Imagem do action" />
+			</figure>
 		</section>
 	);
 }
